df334099e375ba25deea5263097f3cbe218bc0e8
JoJa <jonas-etzold@web.de>
2022-03-11 18:22:53 +0100

    new commit

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import random
-from time import sleep
 
 import pygame
 
 geld = 50
 wins = 0
+gewinn = 0
 einsatz = [1, 2, 3]
 aktueller_einsatz = 0
 
@@ -12,22 +12,17 @@ choice1 = 0
 choice2 = 0
 choice3 = 0
 
-white = (255, 255, 255)
-yellow = (255, 255, 0)
-orange = (255, 100, 0)
+red = (255, 0, 0)
+black = (0, 0, 0)
 
 pygame.init()
 screen = pygame.display.set_mode((600, 960))
 background = pygame.image.load("textures/slot.png")
-font = pygame.font.SysFont('timesnewroman', 250)
-letters = [font.render("A", False, orange, yellow), font.render("B", False, orange, white),
-           font.render("C", False, orange, white), font.render("D", False, orange, white),
-           font.render("E", False, orange, white), font.render("F", False, orange, white),
-           font.render("G", False, orange, white)]
-font = pygame.font.SysFont('timesnewroman', 30)
-letters_bets = font.render(str(einsatz[aktueller_einsatz]) + " €", False, orange, yellow)
-letters_geld = font.render(str(geld) + " €", False, orange, yellow)
-letters_wins = font.render(str(wins), False, orange, yellow)
+pics = [pygame.image.load("textures/diamant.png"), pygame.image.load("textures/sieben.png"),
+        pygame.image.load("textures/bar.png"), pygame.image.load("textures/hufeisen.png"),
+        pygame.image.load("textures/geld.png"), pygame.image.load("textures/glocke.png"),
+        pygame.image.load("textures/cherry.png")]
+font = pygame.font.SysFont('arial', 25)
 pygame.display.set_caption("SlotSim")
 
 
@@ -52,19 +47,19 @@ for symbol in symbols:
     weights.append(symbol.percentage)
 
 
-def symbolChoice():
+def symbol_choice():
     choice = random.randint(0, 100)
-    if (0 <= choice <= 40):
+    if 0 <= choice <= 40:
         return 6
-    elif (41 <= choice <= 65):
+    elif 41 <= choice <= 65:
         return 5
-    elif (66 <= choice <= 80):
+    elif 66 <= choice <= 80:
         return 4
-    elif (81 <= choice <= 90):
+    elif 81 <= choice <= 90:
         return 3
-    elif (91 <= choice <= 97):
+    elif 91 <= choice <= 97:
         return 2
-    elif (98 <= choice <= 99):
+    elif 98 <= choice <= 99:
         return 1
     else:
         return 0
@@ -78,31 +73,37 @@ spiel_aktiv = True
 
 while spiel_aktiv:
 
-    font = pygame.font.SysFont('timesnewroman', 250)
-    letters = [font.render("A", False, orange, yellow), font.render("B", False, orange, white),
-               font.render("C", False, orange, white), font.render("D", False, orange, white),
-               font.render("E", False, orange, white), font.render("F", False, orange, white),
-               font.render("G", False, orange, white)]
-    font = pygame.font.SysFont('timesnewroman', 30)
-    letters_bets = font.render(str(einsatz[aktueller_einsatz]) + " €", False, orange, yellow)
-    letters_geld = font.render(str(geld) + " €", False, orange, yellow)
-    letters_wins = font.render(str(wins), False, orange, yellow)
+    font = pygame.font.SysFont('arial', 28)
+    letters_bets = font.render(str(einsatz[aktueller_einsatz]) + "€", False, red)
+    letters_geld = font.render(str(geld) + "€ (" + str(gewinn) + "€)", False, red)
+    letters_wins = font.render(str(wins), False, red)
 
     # draw frame
     screen.blit(background, (0, 0))
-    screen.blit(letters[choice1], (30, 440))
-    screen.blit(letters[choice2], (270, 440))
-    screen.blit(letters[choice3], (450, 440))
+    screen.blit(pics[choice1], (90, 500))
+    screen.blit(pics[choice2], (265, 500))
+    screen.blit(pics[choice3], (440, 500))
 
-    screen.blit(letters_bets, (150, 930))
-    screen.blit(letters_geld, (380, 930))
-    screen.blit(letters_wins, (650, 930))
+    screen.blit(letters_bets, (90, 695))
+    screen.blit(letters_geld, (215, 695))
+    screen.blit(letters_wins, (430, 695))
     pygame.display.update()
+
+    if geld - einsatz[aktueller_einsatz] < 0:
+        geld = 50
+        aktueller_einsatz = 0
+        wins = 0
+        print("Neuanfang")
+        screen.fill(black)
+        letters_bets = font.render("Dir wird schwarz vor Augen!", False, red)
+        screen.blit()
+        pygame.display.update()
+        pygame.time.delay(1000)
 
     # Überprüfen, ob Nutzer eine Aktion durchgeführt hat
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            spielaktiv = False
+            spiel_aktiv = False
         elif event.type == pygame.KEYDOWN:
             # Abfrage, welche Taste gedrückt wurde
             if event.key == pygame.K_UP:
@@ -114,18 +115,16 @@ while spiel_aktiv:
                     aktueller_einsatz -= 1
                     print("Einsatz gesenkt")
             elif event.key == pygame.K_e:
-                print("Neue Runde")
                 geld -= einsatz[aktueller_einsatz]
-                choice1 = symbolChoice()
-                print(symbols[choice1])
-                choice2 = symbolChoice()
-                print(symbols[choice2])
-                choice3 = symbolChoice()
-                print(symbols[choice3])
+                gewinn = -einsatz[aktueller_einsatz]
+                choice1 = symbol_choice()
+                choice2 = symbol_choice()
+                choice3 = symbol_choice()
                 if choice1 == choice2 == choice3:
-                    print("You won " + str(symbols[choice1].win * einsatz[aktueller_einsatz]))
+                    gewinn = symbols[choice1].win * einsatz[aktueller_einsatz]
+                    print("You won " + str(gewinn) + "€!")
                     wins += 1
-                    geld += symbols[choice1].win * einsatz[aktueller_einsatz]
+                    geld += gewinn
             elif event.key == pygame.K_r:
                 geld = 50
                 aktueller_einsatz = 0
